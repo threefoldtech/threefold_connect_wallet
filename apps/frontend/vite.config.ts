a1301e34d7b84b2febf1600905e8af374b376caa
Jonas Wijne <jonas.wijne@jimber.io>
2022-03-01 17:23:43 +0100

    fix

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,11 +3,9 @@ import vue from '@vitejs/plugin-vue';
 // @ts-ignore
 import path from 'path';
 
-import { visualizer } from 'rollup-plugin-visualizer';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), visualizer()],
+    plugins: [vue()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, '/src'),
@@ -28,52 +26,5 @@ export default defineConfig({
     build: {
         minify: false,
         target: ['ESNext'],
-        rollupOptions: {
-            output: {
-                manualChunks: id => {
-                    if (!id.includes('node_modules')) {
-                        return 'application-bundle';
-                    }
-
-                    if (id.includes('libsodium' || 'tweetnacl')) {
-                        return 'libsodium-tweetnacl';
-                    }
-
-                    if (id.includes('@polkadot')) {
-                        return 'polkadot';
-                    }
-
-                    if (id.includes('@polkadot/types')) {
-                        return 'polkadot/types';
-                    }
-
-                    if (id.includes('@jimber')) {
-                        return 'jimber-bundle';
-                    }
-
-                    if (id.includes('stellar')) {
-                        return 'stellar';
-                    }
-
-                    if (id.includes('node-forge')) {
-                        return 'node-forge';
-                    }
-
-                    if (id.includes('lodash')) {
-                        return 'lodash';
-                    }
-
-                    if (id.includes('headlessui')) {
-                        return '@headlessui';
-                    }
-
-                    if (id.includes('@vue') || id.includes('vue-router')) {
-                        return 'vue';
-                    }
-
-                    return 'vendor';
-                },
-            },
-        },
     },
 });

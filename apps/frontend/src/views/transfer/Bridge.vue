d97b1e79fb643344a8905d9a36a4575388aff270
LennertDefauw <lennert.defauw@jimber.org>
2022-03-09 11:26:18 +0100

    Add loading spinner in bridger view

--- apps/frontend/src/views/transfer/Bridge.vue
+++ apps/frontend/src/views/transfer/Bridge.vue
@@ -22,7 +22,14 @@
                     <div class="text-lg text-gray-500">{{ substrateBalance }} TFT</div>
                 </div>
                 <div class="mt-8">
-                    <label class="block text-sm font-medium text-gray-700" for="amount">Amount</label>
+                    <label class="block text-sm font-medium text-gray-700" for="amount">
+                        <div class="pr-3">
+                            <span class="pr-2">Amount</span>
+                            <span class="text-xs text-gray-400" @click="amount = stellarBalance"
+                                >( {{ stellarBalance }})</span
+                            >
+                        </div>
+                    </label>
                     <div class="relative mt-1 rounded-md shadow-sm">
                         <input
                             id="amount"
@@ -59,14 +66,80 @@
         <div v-else class="h-full w-full">
             <LoadingSpinner></LoadingSpinner>
         </div>
+
+        <div class="" v-if="isLoadingTransaction">
+            <div class="fixed inset-0 flex items-center justify-center"></div>
+            <TransitionRoot appear :show="isLoadingTransaction" as="template">
+                <Dialog as="div">
+                    <div class="fixed inset-0 z-10 overflow-y-auto">
+                        <div class="min-h-screen px-4 text-center">
+                            <TransitionChild
+                                as="template"
+                                enter="duration-300 ease-out"
+                                enter-from="opacity-0"
+                                enter-to="opacity-100"
+                                leave="duration-200 ease-in"
+                                leave-from="opacity-100"
+                                leave-to="opacity-0"
+                            >
+                                <DialogOverlay class="fixed inset-0 bg-slate-500/40" />
+                            </TransitionChild>
+
+                            <span class="inline-block h-screen align-middle" aria-hidden="true"> &#8203; </span>
+
+                            <TransitionChild
+                                as="template"
+                                enter="duration-300 ease-out"
+                                enter-from="opacity-0 scale-95"
+                                enter-to="opacity-100 scale-100"
+                                leave="duration-200 ease-in"
+                                leave-from="opacity-100 scale-100"
+                                leave-to="opacity-0 scale-95"
+                            >
+                                <div
+                                    class="my-8 inline-block w-full max-w-md transform overflow-hidden rounded-md bg-white p-6 text-left align-middle shadow-xl transition-all"
+                                >
+                                    <div class="flex w-full flex-col items-center justify-center">
+                                        <div class="pb-4 text-center">{{ loadingSubtitle }} ...</div>
+                                        <div>
+                                            <svg
+                                                class="h-6 animate-spin text-center text-primary-600"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <circle
+                                                    class="opacity-25"
+                                                    cx="12"
+                                                    cy="12"
+                                                    r="10"
+                                                    stroke="currentColor"
+                                                    stroke-width="4"
+                                                ></circle>
+                                                <path
+                                                    class="opacity-75"
+                                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                                    fill="currentColor"
+                                                ></path>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                </div>
+                            </TransitionChild>
+                        </div>
+                    </div>
+                </Dialog>
+            </TransitionRoot>
+        </div>
     </MainLayout>
 </template>
 
 <script lang="ts" setup>
+    import { TransitionRoot, TransitionChild, DialogOverlay, Dialog, DialogTitle } from '@headlessui/vue';
     import { Wallet, wallets } from '@/service/walletService';
     import { useRoute, useRouter } from 'vue-router';
     import { computed, Ref, ref } from 'vue';
-    import { getSubstrateApi } from '@/service/substrateService';
+    import { activationServiceForSubstrate, getSubstrateApi } from '@/service/substrateService';
     import MainLayout from '@/layouts/MainLayout.vue';
     import PageHeader from '@/components/header/PageHeader.vue';
     import { ArrowLeftIcon, ArrowDownIcon } from '@heroicons/vue/solid';
@@ -82,6 +155,8 @@
     const selectedWallet = ref<Wallet>() as Ref<Wallet>;
 
     const isLoading = ref<boolean>(true);
+    const isLoadingTransaction = ref<boolean>(false);
+    const loadingSubtitle = ref<string>('');
 
     const router = useRouter();
     const route = useRoute();
@@ -147,6 +222,8 @@
     };
 
     const submitBridge = async () => {
+        isLoadingTransaction.value = true;
+
         if (!selectedWallet.value.keyPair) return;
 
         // Check if given amount is valid
@@ -156,18 +233,21 @@
         const substrateAddressTo = selectedWallet.value?.keyPair.getSubstrateKeyring().address;
         const api = await getSubstrateApi();
 
+        loadingSubtitle.value = 'Using activation service';
         console.info('Using activation service for substrate');
-        // await activationServiceForSubstrate(substrateAddressTo);
+        await activationServiceForSubstrate(substrateAddressTo);
 
         const substrateKeyRing = selectedWallet.value.keyPair.getSubstrateKeyring();
         const name = userInitialized.value;
 
         if (!name) return;
 
+        loadingSubtitle.value = 'Getting entity ID';
         console.info('Getting entityId for user ', name);
         let entityId = await getEntityIDByName(api, name);
 
         if (entityId == 0) {
+            loadingSubtitle.value = 'Entity not found, creating one';
             console.info("Can't find entity, creating one");
 
             const country = 'Unknown';
@@ -182,6 +262,7 @@
             console.info('Created nonce: ', nonce.toHuman);
 
             const signAndSendCallback = async (res: any) => {
+                loadingSubtitle.value = 'Transacting the funds';
                 console.info('Callback from signAndSend.');
 
                 if (res instanceof Error) {
@@ -193,6 +274,7 @@
                 console.info(`Current status is ${status.type}`);
 
                 if (status.isFinalized) {
+                    loadingSubtitle.value = 'Transaction done';
                     console.info(`Transaction included at blockHash ${status.asFinalized}`);
 
                     // @ts-ignore
@@ -211,7 +293,8 @@
         }
 
         await bridgeToSubstrate(amount.value, selectedWallet.value.keyPair.getStellarKeyPair(), entityId);
-
+        loadingSubtitle.value = 'Finishing up';
+        isLoadingTransaction.value = false;
         console.log('Transaction done');
     };
 

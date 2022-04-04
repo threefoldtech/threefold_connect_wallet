fea639a0850fa0767d872b460943c276d98ebd14
Jonas Wijne <jonas.wijne@jimber.io>
2022-04-04 15:46:35 +0200

    improve bundle size

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import vue from '@vitejs/plugin-vue';
 // @ts-ignore
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), splitVendorChunkPlugin()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, '/src'),
@@ -24,7 +24,7 @@ export default defineConfig({
     },
 
     build: {
-        minify: false,
+        minify: 'terser',
         target: ['ESNext'],
     },
 });

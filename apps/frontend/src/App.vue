b2d333e88735876ff6074486d7dc1964f20d8655
Jonas Wijne <jonas.wijne@jimber.io>
2022-04-26 10:51:02 +0200

    make additional dev banner

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -1,6 +1,13 @@
 <template>
     <div
-        v-if="isBeta"
+        v-if="isDev"
+        class="user-select-none pointer-events-none fixed right-0 mt-9 mr-9 w-72 origin-top bg-yellow-500 text-center opacity-80"
+        style="transform: translateX(50%) rotate(45deg)"
+    >
+        <div>DEV</div>
+    </div>
+    <div
+        v-if="isBeta && !isDev"
         class="user-select-none pointer-events-none fixed right-0 mt-9 mr-9 w-72 origin-top bg-yellow-500 text-center opacity-80"
         style="transform: translateX(50%) rotate(45deg)"
     >

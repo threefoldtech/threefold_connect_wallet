b241f1967f03e4bb773caefcf17956f2a415ba93
LennertDefauw <lennert.defauw@jimber.org>
2022-05-02 21:42:14 +0200

    Update retries in currency service

--- apps/frontend/src/modules/Currency/services/currencyService.ts
+++ apps/frontend/src/modules/Currency/services/currencyService.ts
@@ -1,7 +1,7 @@
 import { getSubstrateAssetBalances } from '@/modules/TFChain/services/tfchainService';
 
 export const isSubstrateBalanceAvailable = async (id: string, retries = 0) => {
-    while (retries > 5) {
+    while (retries > 10) {
         const substrateBalance = await getSubstrateAssetBalances(id);
         if (substrateBalance.filter(b => b.amount >= 0).length > 0) {
             return true;

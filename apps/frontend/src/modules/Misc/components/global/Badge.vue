2f4be081db22e07880ea9a75db46309db6fbd9bc
LennertDefauw <lennert.defauw@jimber.org>
2022-05-16 13:51:44 +0200

    Improvement :class

--- apps/frontend/src/modules/Misc/components/global/Badge.vue
+++ apps/frontend/src/modules/Misc/components/global/Badge.vue
@@ -1,5 +1,14 @@
 <template>
-    <span class="mr-2 rounded px-2.5 py-0.5 text-sm font-medium" :class="defineCorrectType(type)"> {{ text }}</span>
+    <span
+        class="mr-2 rounded px-2.5 py-0.5 text-sm font-medium"
+        :class="{
+            'bg-green-100 text-green-800 dark:bg-green-200 dark:text-green-900': type === BadgeType.POSITIVE,
+            'bg-red-100 text-red-800 dark:bg-red-200 dark:text-red-900': type === BadgeType.NEGATIVE,
+            'bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-900': type === BadgeType.NEUTRAL,
+        }"
+    >
+        {{ text }}</span
+    >
 </template>
 
 <script lang="ts" setup>
@@ -11,17 +20,6 @@
     }
 
     const { type, text } = defineProps<IProps>();
-
-    const defineCorrectType = (type: BadgeType) => {
-        switch (type) {
-            case BadgeType.POSITIVE:
-                return 'bg-green-100 text-green-800 dark:bg-green-200 dark:text-green-900';
-            case BadgeType.NEGATIVE:
-                return 'bg-red-100 text-red-800 dark:bg-red-200 dark:text-red-900';
-            case BadgeType.NEUTRAL:
-                return 'bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-900';
-        }
-    };
 </script>
 
 <style scoped></style>

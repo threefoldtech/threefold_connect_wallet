651de4049a6196954ca90e930b96f8536ad75b44
LennertDefauw <lennert.defauw@jimber.org>
2022-10-24 11:24:55 +0200

    Hotfix on development

--- packages/substrate/src/states/grid.state.ts
+++ packages/substrate/src/states/grid.state.ts
@@ -2,20 +2,11 @@ import { getSubstrateApi } from '../services/core.service.substrate';
 import { ITermsAndConditions } from 'shared-types/src/interfaces/substrate/tac.interfaces';
 import { bin2String } from 'wallet-frontend/src/modules/Core/utils/crypto';
 
-export const getEntityIdByAccountId = async (accountId: string): Promise<number> => {
-    const api = await getSubstrateApi();
-
-    const entity = await api.query.tfgridModule.entityIdByAccountID(accountId);
-    const entityId = entity.toJSON();
-
-    return entityId as number;
-};
-
 // User terms and conditions not available on GQL
 export const getUsersTermsAndConditionsByAccountId = async (accountId: string): Promise<ITermsAndConditions[] | []> => {
     const api = await getSubstrateApi();
 
-    const userTermsAndConditions = await api.query.tfgridModule.usersTermsAndConditions(accountId);
+    const userTermsAndConditions = (await api.query.tfgridModule.usersTermsAndConditions(accountId)).toHuman();
 
     if (!userTermsAndConditions) {
         return [];
@@ -24,8 +15,8 @@ export const getUsersTermsAndConditionsByAccountId = async (accountId: string): 
     //@ts-ignore
     return userTermsAndConditions.map((term: any) => {
         return {
-            documentHash: bin2String(term.document_hash),
-            documentLink: bin2String(term.document_link),
+            documentHash: bin2String(term.documentHash),
+            documentLink: bin2String(term.documentLink),
         };
     });
 };
